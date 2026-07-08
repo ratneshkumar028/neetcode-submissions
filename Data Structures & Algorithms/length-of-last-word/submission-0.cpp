15e6ebeea0e97aaadd376c73c77d7efd90134867
Ratnesk kumar <149929407+ratneshkumar028@users.noreply.github.com>
2026-07-08 21:21:10 +0530

    Add: length-of-last-word - submission-0

---
+++ Data Structures & Algorithms/length-of-last-word/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.size();
+        int l = 0;
+        int i =n-1;
+        while(i>=0 && s[i]==' '){
+            i--;
+        }
+        while(i>=0 && s[i]!=' '){
+            l++;
+            i--;
+        }
+        return l;
+    }
+};
