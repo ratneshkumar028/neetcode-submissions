d2990d2cb989e07aafe3dc4eb1a26635b18eec8f
Ratnesk kumar <149929407+ratneshkumar028@users.noreply.github.com>
2026-08-13 22:33:28 +0530

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int mod = 1e9;
+        vector<int> dp(amount+1, mod);
+        dp[0]=0;
+        for( int i=1;i<=amount;i++){
+            for( int coin : coins){
+                if(i-coin>=0){
+                    dp[i] = min(dp[i],1+dp[i-coin]);
+                }
+            }
+        }
+        return dp[amount] == mod ? -1: dp[amount];
+    }
+};
